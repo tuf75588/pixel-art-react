c6928127794d06fd44b17e5436dc37f7c89275fd
Andrew Davis <atd285@gmail.com>
2020-09-02 23:06:43 -0400

    application state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function App() {
+function App(): JSX.Element {
+  const [background, setBackground] = React.useState(
+    'rgb(255, 255, 255) none repeat scroll 0% 0%;'
+  );
   return (
     <div className="App container">
       <main className="grid-container">
-        {Array.from({ length: 40 }, (_, i) => {
-          return <div key={i} children={i + 1} className="square" />;
+        {Array.from({ length: 40 }, (_, i: number) => {
+          return (
+            <div
+              key={i}
+              className="square"
+              style={{ backgroundColor: background }}
+            />
+          );
         })}
       </main>
     </div>
